bb53c1a8c8a6280e4c29ca6c5301b0d097854773
Sam Berven <samberven@email.com>
2023-12-01 13:11:40 +0900

    Incorporate microservice; add backup files

---
+++ backend/backupClient.js
@@ -0,0 +1,38 @@
+import zmq from 'zeromq';
+
+// import json from 'json';
+import express from "express";
+const client = express()
+client.use(express.json())
+
+// const zmq = require("zeromq");
+// const json = require("json");
+
+export async function copyData(dataDict) {
+  const socket = new zmq.Request();
+
+  await socket.connect("tcp://localhost:5555");
+
+  const json_data = JSON.stringify(dataDict);
+  socket.send(json_data);
+
+
+  const [response] = await socket.receive();
+  console.log("Received:", response.toString());
+
+  await socket.close();
+
+  return response.toString();
+}
+
+// const data = {
+//   readyForCopying: true,
+//   json_data: {
+//     name: "pizza",
+//     description: "delicious",
+//     category: "food",
+//     price: "20",
+//   },
+// };
+
+// copyData(data);
