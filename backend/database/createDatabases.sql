293d3bc952f1d8dbc5cb110408792eb2daa71f6c
Sam Berven <samberven@email.com>
2023-12-08 10:08:47 -0800

    Update DB starter data

--- backend/database/createDatabases.sql
+++ backend/database/createDatabases.sql
@@ -1,3 +1,7 @@
+-- Create Kedama and customerFacing tables
+
+
+-- Create Kedama products table
 create table products (
 id int auto_increment,
 name varchar(255) not null,
@@ -7,6 +11,7 @@ price decimal(15,2) not null,
 primary key (id));
 
 
+-- Kedama "products" table starter data
 INSERT INTO products
 (name, description, category, price)
 VALUES('Pastel Sweater', 'A sweater featuring beautiful pastel colors', 'Outerwear', 29.99);
@@ -14,3 +19,22 @@ VALUES('Pastel Sweater', 'A sweater featuring beautiful pastel colors', 'Outerwe
 INSERT INTO products
 (name, description, category, price)
 VALUES('Pink Jeans', 'Bold jeans with shocking pink denim', 'Bottoms', 49.99);
+
+
+-- Create Kedama "customerFacing" table
+create table products (
+id int auto_increment,
+name varchar(255) not null,
+description varchar(255) not null,
+price decimal(15,2) not null,
+primary key (id));
+
+
+-- Kedama "customerFacing" table start data
+INSERT INTO products
+(name, description, price)
+VALUES('Pastel Sweater', 'A sweater featuring beautiful pastel colors', 29.99);
+
+INSERT INTO products
+(name, description, price)
+VALUES('Pink Jeans', 'Bold jeans with shocking pink denim', 49.99);
